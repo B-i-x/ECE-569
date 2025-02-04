34bb1f713877daedfb4ac3ce6433403148b80680
B-i-x <bix.romero@gmail.com>
2025-02-04 09:31:05 -0700

    setup in github

---
+++ in_class_activities/1-04-class/runner.bat
@@ -0,0 +1,15 @@
+@echo off
+REM Load the CUDA environment (if required)
+CALL nvcc -o myadd.exe ..\madd_template.cu -ccbin "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64"
+
+REM Check if compilation was successful
+IF %ERRORLEVEL% NEQ 0 (
+    echo Compilation failed.
+    GOTO :end
+)
+
+echo Compilation successful, running the program...
+myadd.exe
+
+:end
+pause
