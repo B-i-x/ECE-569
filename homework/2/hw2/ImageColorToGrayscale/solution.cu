338b3f751f3f8bee01824f17039fc309afdcb601
B-i-x <bix.romero@gmail.com>
2025-02-19 19:15:24 -0700

    doing image file

--- homework/2/hw2/ImageColorToGrayscale/solution.cu
+++ homework/2/hw2/ImageColorToGrayscale/solution.cu
@@ -10,8 +10,19 @@
     }                                                                     \
   } while (0)
 
-//@@ INSERT DEVICE CODE HERE
-// Also modify the main function to launch thekernel. 
+// Kernel: Convert RGB image to grayscale
+__global__ void rgb2gray(const float *input, float *output, int width, int height) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  int totalPixels = width * height;
+  if (idx < totalPixels) {
+    int rgbIdx = idx * 3;
+    float r = input[rgbIdx];
+    float g = input[rgbIdx + 1];
+    float b = input[rgbIdx + 2];
+    output[idx] = 0.21f * r + 0.71f * g + 0.07f * b;
+  }
+}
+
 int main(int argc, char *argv[]) {
   wbArg_t args;
   int imageChannels;
@@ -26,17 +37,15 @@ int main(int argc, char *argv[]) {
   float *deviceOutputImageData;
 
   args = wbArg_read(argc, argv); /* parse the input arguments */
-
   inputImageFile = wbArg_getInputFile(args, 0);
 
   inputImage = wbImport(inputImageFile);
-
   imageWidth  = wbImage_getWidth(inputImage);
   imageHeight = wbImage_getHeight(inputImage);
-  // For this lab the value is always 3
+  // For this lab the value is always 3 (RGB channels)
   imageChannels = wbImage_getChannels(inputImage);
 
-  // Since the image is monochromatic, it only contains one channel
+  // Since the output image is monochromatic, it only contains one channel
   outputImage = wbImage_new(imageWidth, imageHeight, 1);
 
   hostInputImageData  = wbImage_getData(inputImage);
@@ -44,6 +53,7 @@ int main(int argc, char *argv[]) {
 
   wbTime_start(GPU, "Doing GPU Computation (memory + compute)");
 
+  // Allocate GPU memory
   wbTime_start(GPU, "Doing GPU memory allocation");
   cudaMalloc((void **)&deviceInputImageData,
              imageWidth * imageHeight * imageChannels * sizeof(float));
@@ -51,6 +61,7 @@ int main(int argc, char *argv[]) {
              imageWidth * imageHeight * sizeof(float));
   wbTime_stop(GPU, "Doing GPU memory allocation");
 
+  // Copy host memory to GPU
   wbTime_start(Copy, "Copying data to the GPU");
   cudaMemcpy(deviceInputImageData, hostInputImageData,
              imageWidth * imageHeight * imageChannels * sizeof(float),
@@ -58,12 +69,18 @@ int main(int argc, char *argv[]) {
   wbTime_stop(Copy, "Copying data to the GPU");
 
   ///////////////////////////////////////////////////////
+  // Launch the kernel to convert the image to grayscale
   wbTime_start(Compute, "Doing the computation on the GPU");
-  //@@ INSERT CODE HERE
-
+  int numPixels = imageWidth * imageHeight;
+  int blockSize = 256;
+  int gridSize = (numPixels + blockSize - 1) / blockSize;
+  rgb2gray<<<gridSize, blockSize>>>(deviceInputImageData, deviceOutputImageData,
+                                    imageWidth, imageHeight);
+  cudaDeviceSynchronize();
   wbTime_stop(Compute, "Doing the computation on the GPU");
 
   ///////////////////////////////////////////////////////
+  // Copy the result from GPU to CPU
   wbTime_start(Copy, "Copying data from the GPU");
   cudaMemcpy(hostOutputImageData, deviceOutputImageData,
              imageWidth * imageHeight * sizeof(float),
@@ -74,6 +91,7 @@ int main(int argc, char *argv[]) {
 
   wbSolution(args, outputImage);
 
+  // Free allocated GPU memory
   cudaFree(deviceInputImageData);
   cudaFree(deviceOutputImageData);
 
