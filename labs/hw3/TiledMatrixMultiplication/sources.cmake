34cd8c465338749926912623157a33780f8f442b
B-i-x <bix.romero@gmail.com>
2025-03-06 00:55:13 -0700

    updating_output

--- labs/hw3/TiledMatrixMultiplication/sources.cmake
+++ labs/hw3/TiledMatrixMultiplication/sources.cmake
@@ -1,4 +1,4 @@
 
 add_lab("TiledMatrixMultiplication")
-add_lab_solution("TiledMatrixMultiplication" ${CMAKE_CURRENT_LIST_DIR}/no_timer_util.cu)
+add_lab_solution("TiledMatrixMultiplication" ${CMAKE_CURRENT_LIST_DIR}/solution.cu)
 add_generator("TiledMatrixMultiplication" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.cpp)
