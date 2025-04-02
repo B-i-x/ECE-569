70af6e9c8c6a61627b38350b7353c6e9f9a8d448
B-i-x <bix.romero@gmail.com>
2025-04-02 15:14:17 -0700

    1024 is to mcu

--- labs/hw4/Histogram/solution.cu
+++ labs/hw4/Histogram/solution.cu
@@ -102,7 +102,7 @@ void histogram(
   }
 
   // ----- Step 2: Sort the input using Bitonic Sort -----
-  int block_size = 1024;
+  int block_size = 512;
   int grid_size = (padLength + block_size - 1) / block_size;
   // Outer loop over subsequence sizes: k doubles each time.
   for (unsigned int k = 2; k <= padLength; k <<= 1) {
