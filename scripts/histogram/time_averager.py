22999a83a48d7b46af2bbad8d5e742d1eebd126c
B-i-x <bix.romero@gmail.com>
2025-04-02 11:53:18 -0700

    fixing script mroe

--- scripts/histogram/time_averager.py
+++ scripts/histogram/time_averager.py
@@ -3,12 +3,14 @@ import re
 import numpy as np
 
 # Define your parameters here
-version_number_input = 2
-base_path = f"outputs/histogram/v{version_number_input}/repeat"
+VERSION_NUM = 2
+INITIAL_DATASET_INDEX = 0
+FINAL_DATASET_INDEX = 1
+RUN_NUM = 10
 
-first_dataset = 0
-last_dataset = 1
-num_runs = 10
+base_path = f"outputs/histogram/v{VERSION_NUM}/repeat"
+
+
 
 # Regular expression to match the compute time line
 compute_time_regex = re.compile(r"Total compute time \(ms\) ([0-9\.]+)")
@@ -16,10 +18,10 @@ compute_time_regex = re.compile(r"Total compute time \(ms\) ([0-9\.]+)")
 # Store averaged times per dataset
 dataset_avg_times = []
 
-for dataset in range(first_dataset, last_dataset):
+for dataset in range(INITIAL_DATASET_INDEX, FINAL_DATASET_INDEX):
     print(f"Processing dataset {dataset}...")
     times = []
-    for run in range(1, num_runs + 1):
+    for run in range(1, RUN_NUM + 1):
         file_path = os.path.join(base_path, f"output{dataset}_run{run}.txt")
         with open(file_path, 'r') as file:
             for line in file:
